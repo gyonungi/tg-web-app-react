af990277ee282249e1ab7aaf73b8b0345532de63
gyonungi <104500843+gyonungi@users.noreply.github.com>
2023-08-04 21:47:52 +0700

    componet button

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from "../Button/Button";
+import Button from "../button/button";
 import "./header.css";
 import { useTelegram } from "../../hooks/useTelegram";
 
