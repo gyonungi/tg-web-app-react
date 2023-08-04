7dca6441269b4bbf7a18c6170f9e672f304f8120
gyonungi <104500843+gyonungi@users.noreply.github.com>
2023-08-04 21:46:27 +0700

    fixed styles

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Button from "../Button/Button";
-
-import "./Header.css";
+import "./header.css";
 import { useTelegram } from "../../hooks/useTelegram";
 
 const Header = () => {
