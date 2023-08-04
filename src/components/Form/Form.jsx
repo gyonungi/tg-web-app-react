c02629392be536f41a99ae97c412eab003ca1b72
gyonungi <104500843+gyonungi@users.noreply.github.com>
2023-08-04 22:39:17 +0700

    edd styles

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -29,23 +29,23 @@ const Form = () => {
   };
   return (
     <>
-      <div className={"form"}>
+      <div className="form">
         <h3>Введите ваши данные</h3>
         <input
-          className={"input"}
+          className="input"
           type="text"
           placeholder={"Страна"}
           value={country}
           onChange={onChangeCountry}
         />
         <input
-          className={"input"}
+          className="input"
           type="text"
           placeholder={"Улица"}
           value={city}
           onChange={onChangeCity}
         />
-        <select className={"select"} value={subject} onChange={onChangeSubject}>
+        <select className="select" value={subject} onChange={onChangeSubject}>
           <option value={"physical"}>Физ.лицо</option>
           <option value={"legal"}>Юр.лицо</option>
         </select>
