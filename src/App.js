c32cbd89cbee6ed6969ed0c8ae74563bcd5eeba2
gyonungi <104500843+gyonungi@users.noreply.github.com>
2023-08-04 21:29:27 +0700

    fixed hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
 import "./App.css";
-import useTelegram from "./hooks/useTelegram";
+import { useTelegram } from "./hooks/useTelegram";
 
 function App() {
   const { onToggleButton, tg } = useTelegram();
+
   useEffect(() => {
     tg.ready();
   }, []);
-
   return (
     <div className="App">
       <button onClick={onToggleButton}>toggle</button>
